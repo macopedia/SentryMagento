c0a48daab2c05b26cf8721f0f5850daf3f5f5e6f
Lucas van Staden <sales@proxiblue.com.au>
2018-08-04 20:53:31 +0800

    adjust to allow ignoreErrors. Ability to set raven url and allow different (update) of raven.js used.

--- app/code/local/Macopedia/Sentry/Block/Sentry.php
+++ app/code/local/Macopedia/Sentry/Block/Sentry.php
@@ -26,12 +26,21 @@ class Macopedia_Sentry_Block_Sentry extends Mage_Core_Block_Template
         return Mage::getStoreConfig('macopedia_sentry/general/env_type');
     }
 
+    public function getRavenUrl()
+    {
+        return Mage::getStoreConfig('macopedia_sentry/general/raven_js_url');
+    }
+
     public function getOptions() {
         $options = ['environment' => $this->getEnvType() ];
         if(!empty($whiteListRaw = Mage::getStoreConfig('macopedia_sentry/general/whitelist_urls'))) {
-            $whiteLists = explode(",", $whiteListRaw);
-            $options['whitelistUrls'] = $whiteLists;
+            $whiteLists = explode(PHP_EOL, $whiteListRaw);
+            $options['whitelistUrls'] = array_map('trim', $whiteLists);
         }
-        return json_encode($options);
+        if(!empty($ignoreListRaw = Mage::getStoreConfig('macopedia_sentry/general/ignore_textlist'))) {
+            $ignoreLists = explode(PHP_EOL, $ignoreListRaw);
+            $options['ignoreErrors'] = array_map('trim', $ignoreLists);
+        }
+        return json_encode($options, JSON_UNESCAPED_SLASHES);
     }
 }
