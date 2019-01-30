a7cefc1e2bcd8b22982d32dc9fcfb5ed8039028f
Lucas van Staden <lucas.vanstaden@enjo.com.au>
2019-01-30 07:39:09 +0000

    move filter to Sentry Client just prior to captur

--- app/code/local/Macopedia/Sentry/Helper/Data.php
+++ app/code/local/Macopedia/Sentry/Helper/Data.php
@@ -18,11 +18,33 @@ class Macopedia_Sentry_Helper_Data extends Mage_Core_Helper_Abstract
     public function reportMessage($message)
     {
         try {
-            $message = substr(str_replace("\n",'',$message),0,255);
             $ignoreList = explode("\r\n", Mage::getStoreConfig('macopedia_sentry/patch/ignore_strings'));
             if (is_array($ignoreList) && count($ignoreList) > 0) {
-                $result = $this->ignoreInString($message, $ignoreList);
-                return $result;
+                if (is_array($message)
+                    && isset($message['exception'])
+                    && is_array($message['exception'])
+                    && isset($message['exception']['values'])
+                    && is_array($message['exception']['values'])
+                ) {
+
+
+                    foreach ($message['exception']['values'] as $key => $messageData) {
+                        if (isset($messageData['stacktrace'])) {
+                            unset($messageData['stacktrace']);
+                        }
+                        if (!$this->ignoreInString(implode(' ', $messageData), $ignoreList)) {
+                            unset($message['exception']['values'][$key]);
+                        }
+                    }
+                    if (count($message['exception']['values']) == 0) {
+                        return false;
+                    }
+
+                } elseif (isset($message['message'])) {
+                    if (!$this->ignoreInString($message['message'], $ignoreList)) {
+                        return false;
+                    }
+                }
             }
         } catch (Exception $e) {
             return true;
